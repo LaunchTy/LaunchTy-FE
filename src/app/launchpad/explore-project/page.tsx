2ad87e7641b483c1f2898a1acd6a9b27f87cbf8f
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2025-05-12 14:32:05 +0700

    chore(): update path for explore project

--- src/app/launchpad/explore-project/page.tsx
+++ src/app/launchpad/explore-project/page.tsx
@@ -3,15 +3,14 @@ import ExploreProject from '@/components/Launchpad/Explore-section/ExploreProjec
 import NavBar from '@/components/Launchpad/Explore-section/NavBar'
 import ProjectSection from '@/components/Launchpad/Explore-section/ProjectSection'
 
-
 const ExploreProjectPage = () => {
-  return (
-    <div className="min-h-screen font-exo">
-      <ExploreProject />
-      <NavBar />
-      <ProjectSection/>
-    </div>
-  )
+	return (
+		<div className="min-h-screen font-exo">
+			<ExploreProject />
+			<NavBar />
+			<ProjectSection />
+		</div>
+	)
 }
 
-export default ExploreProjectPage 
+export default ExploreProjectPage
