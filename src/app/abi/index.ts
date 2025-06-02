960807a4133db5643cf2dde4a3d0037bb8b7f79d
notlongfen <phannguyenhoanglong@gmail.com>
2025-06-02 17:02:36 +0700

    feat(abi): add contracts abis

---
+++ src/app/abi/index.ts
@@ -0,0 +1,19 @@
+import LaunchpadFactory from "./LaunchpadFactory.json";
+import Launchpad from "./Launchpad.json";
+import CharityFactory from "./CharityFactory.json";
+import Charity from "./Charity.json";
+import MockERC20 from "./MockERC20.json";
+
+const LaunchpadFactoryABI = LaunchpadFactory.abi;
+const LaunchpadABI = Launchpad.abi;
+const CharityFactoryABI = CharityFactory.abi;
+const CharityABI = Charity.abi;
+const MockERC20ABI = MockERC20.abi;
+
+export {
+	LaunchpadFactoryABI,
+	LaunchpadABI,
+	CharityFactoryABI,
+	CharityABI,
+	MockERC20ABI,
+};
