a344c703b913cc64b1b83fab2fb226e5137623c5
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2025-06-09 11:44:09 +0700

    feat(): add deposit and donate api

---
+++ src/app/api/charity/donate/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import prismaClient from "@/prisma";
+
+export async function POST(req: NextRequest) {
+	try {
+		const body = await req.json();
+		const { charity_id, wallet_address, donate_amount, tx_hash } = body;
+		console.log("Received data:", body);
+
+		if (!charity_id || !wallet_address || !donate_amount) {
+			return NextResponse.json(
+				{ success: false, error: "Missing required fields" },
+				{ status: 400 }
+			);
+		}
+
+		const user = await prismaClient.user.findUnique({
+			where: { wallet_address },
+		});
+		console.log("User found:", user);
+
+		if (!user) {
+			return NextResponse.json(
+				{ success: false, error: "User not found" },
+				{ status: 404 }
+			);
+		}
+
+		const newDonation = await prismaClient.donation.create({
+			data: {
+				amount: donate_amount,
+				tx_hash: tx_hash,
+				user_id: user.user_id,
+				charity_id,
+			},
+		});
+
+		return NextResponse.json(
+			{ success: true, data: newDonation },
+			{ status: 200 }
+		);
+	} catch (error) {
+		console.error("Error creating donation:", error);
+		return NextResponse.json(
+			{ success: false, error: "Internal Server Error" },
+			{ status: 500 }
+		);
+	}
+}
---
+++ src/app/api/launchpad/deposit/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+import prismaClient from "@/prisma";
+
+export async function POST(req: NextRequest) {
+	try {
+		const body = await req.json();
+		const { launchpad_id, wallet_address, deposit_amount, tx_hash } = body;
+
+		if (!launchpad_id || !wallet_address || !deposit_amount) {
+			return NextResponse.json(
+				{ success: false, error: "Missing required fields" },
+				{ status: 400 }
+			);
+		}
+
+		const user = await prismaClient.user.findUnique({
+			where: { wallet_address },
+		});
+
+		if (!user) {
+			return NextResponse.json(
+				{ success: false, error: "User not found" },
+				{ status: 404 }
+			);
+		}
+
+		const newDeposit = await prismaClient.deposit.create({
+			data: {
+				amount: deposit_amount,
+				tx_hash: tx_hash,
+				user_id: user.user_id,
+				launchpad_id,
+			},
+		});
+
+		return NextResponse.json(
+			{ success: true, data: newDeposit },
+			{ status: 201 }
+		);
+	} catch (error) {
+		console.error("Error creating deposit:", error);
+		return NextResponse.json(
+			{ success: false, error: "Internal Server Error" },
+			{ status: 500 }
+		);
+	}
+}
